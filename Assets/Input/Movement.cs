3b6f1f9df420fb947ad8b0def844d3cff6f5f319
NathanLacey <nathan.lacey@hotmail.com>
2020-12-08 10:33:35 -0800

    Add base input system stuff

---
+++ Assets/Input/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public float MoveSpeed = 1;
+    Rigidbody2D mRigidbody;
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        mRigidbody.AddForce(context.action.ReadValue<Vector2>() * MoveSpeed, ForceMode2D.Impulse);
+    }
+}
