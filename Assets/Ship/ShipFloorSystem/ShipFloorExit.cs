ff435e6de51a4094c4890ef7c345fd755ce0e132
NathanLacey <nathan.lacey@hotmail.com>
2020-12-09 12:34:11 -0800

    Start of ship floor transition system

---
+++ Assets/Ship/ShipFloorSystem/ShipFloorExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipFloorExit : MonoBehaviour
+{
+	GameObject ThisShipFloor;
+	[SerializeField] GameObject ShipFloorToExitTo;
+	ShipFloorManager FloorManager;
+
+	private void Start()
+	{
+		ThisShipFloor = transform.parent.gameObject;
+		FloorManager = FindObjectOfType<ShipFloorManager>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "Player")
+		{
+			FloorManager.ChangeToShipFloor(ShipFloorToExitTo);
+		}
+	}
+}
---
+++ Assets/Ship/ShipFloorSystem/ShipFloorManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipFloorManager : MonoBehaviour
+{
+	[SerializeField] GameObject CurrentShipFloor;
+    [SerializeField] List<GameObject> ShipFloors = new List<GameObject>();
+
+    void Start()
+	{
+        foreach(GameObject shipFloor in ShipFloors)
+		{
+			if(shipFloor != CurrentShipFloor)
+			{
+				shipFloor.SetActive(false);
+			}
+		}
+		CurrentShipFloor.SetActive(true);
+	}
+
+	public void ChangeToShipFloor(GameObject shipFloorToChangeTo)
+	{
+		if(CurrentShipFloor == shipFloorToChangeTo)
+		{
+			return;
+		}
+		
+		CurrentShipFloor.SetActive(false);
+		CurrentShipFloor = shipFloorToChangeTo;
+		shipFloorToChangeTo.SetActive(true);
+	}
+}
