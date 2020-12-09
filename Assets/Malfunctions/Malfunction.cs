2c04b52aa2e6ab7f472feceab4d6650b4a3bdd58
Jake Roman-Barnes <JakeRomanBarnes@outlook.com>
2020-12-09 15:23:51 -0800

    Added minigame connection interface to malfunctions

--- Assets/Malfunctions/Malfunction.cs
+++ Assets/Malfunctions/Malfunction.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Malfunction : MonoBehaviour
 {
 	[SerializeField] [Range(0.0f, 1.0f)] float activationChance;
+	[SerializeField] GameObject miniGameObject;
 	bool isActive = false;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -15,7 +17,18 @@ public class Malfunction : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-
+		if (isActive)
+		{
+			IMiniGame miniGameComponent;
+			if (miniGameObject && miniGameObject.TryGetComponent<IMiniGame>(out miniGameComponent))
+			{
+				if (miniGameComponent.IsFinished)
+				{
+					isActive = false;
+					gameObject.SetActive(false);
+				}
+			}
+		}
 	}
 
 	// What happens when the player tries to fix it
@@ -24,8 +37,18 @@ public class Malfunction : MonoBehaviour
 		if (isActive)
 		{
 			// hook into the minigame and disable when completed
-			isActive = false;
-			gameObject.SetActive(false);
+			IMiniGame miniGameComponent;
+			if (miniGameObject && miniGameObject.TryGetComponent<IMiniGame>(out miniGameComponent))
+			{
+				// TODO: check that we're not already running the minigame
+				miniGameComponent.StartMiniGame();
+			}
+			// if there's no minigame just disable the malfunction
+			else
+			{
+				isActive = false;
+				gameObject.SetActive(false);
+			}
 		}
 	}
 
@@ -37,6 +60,7 @@ public class Malfunction : MonoBehaviour
 		gameObject.SetActive(true);
 	}
 
+	// called by the manager to start the malfunction
 	public bool TryActivate(System.Random random)
 	{
 		if (!isActive && random.NextDouble() <= activationChance)
