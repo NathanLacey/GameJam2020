8ede2d24473566bc0cae800aed4d404c83905cd0
NathanLacey <nathan.lacey@hotmail.com>
2020-12-09 11:27:34 -0800

    Started working on operation game

---
+++ Assets/Input/IsMouseOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class IsMouseOver : MonoBehaviour
+{
+	[SerializeField] string ObjectTag;
+
+	[HideInInspector] public bool MouseIsOver = false;
+	
+	private void Update()
+	{
+		CheckIfMouseIsOver();
+	}
+
+	void CheckIfMouseIsOver()
+	{
+		Vector2 mousePosition = Mouse.current.position.ReadValue();
+		var ray = Camera.main.ScreenPointToRay(mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 1000))
+		{
+			if (hit.collider.tag == ObjectTag)
+			{
+				MouseIsOver = true;
+				return;
+			}
+		}
+
+		MouseIsOver = false;
+	}
+}
