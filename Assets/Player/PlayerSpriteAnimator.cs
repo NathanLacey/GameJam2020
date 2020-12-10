618838f3eb4d3c55504c472a91419a8e318a60fd
NathanLacey <nathan.lacey@hotmail.com>
2020-12-10 11:43:30 -0800

    Player now animates

--- Assets/Player/PlayerSpriteAnimator.cs
+++ Assets/Player/PlayerSpriteAnimator.cs
@@ -6,9 +6,13 @@ public class PlayerSpriteAnimator : MonoBehaviour
 {
 	[SerializeField] Animator PlayerAnimator;
 	[SerializeField] FacingDirection Rotator;
+	[SerializeField] Movement PlayerMovement;
 	void Update()
 	{
 		float playerAngle = Rotator.transform.rotation.eulerAngles.z;
-		PlayerAnimator.SetInteger("Direction", (int)((playerAngle + 45.0f) / 90.0f) % 4);
+		int playerDirection = (int)((playerAngle + 45.0f) / 90.0f) % 4;
+		PlayerAnimator.SetFloat("Angle", playerDirection / 3f);
+		float isMoving = PlayerMovement.MoveDirection.sqrMagnitude;
+		PlayerAnimator.SetBool("IsWalking", isMoving > 0f);
 	}
 }
