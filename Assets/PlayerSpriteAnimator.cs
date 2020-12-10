18036dde68e8538c2cfa7a9e0a919091950a43bf
NathanLacey <nathan.lacey@hotmail.com>
2020-12-09 17:25:39 -0800

    Did some shenanigans with a player sprite so it looks in the direction your mouse is

---
+++ Assets/PlayerSpriteAnimator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteAnimator : MonoBehaviour
+{
+	[SerializeField] Animator PlayerAnimator;
+	[SerializeField] FacingDirection Rotator;
+	void Update()
+	{
+		float playerAngle = Rotator.transform.rotation.eulerAngles.z;
+		PlayerAnimator.SetInteger("Direction", (int)((playerAngle + 45.0f) / 90.0f) % 4);
+	}
+}
