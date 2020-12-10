965cb3eea206dbf4e65c83bb6a562f3563fbe0fe
NathanLacey <nathan.lacey@hotmail.com>
2020-12-10 15:54:38 -0800

    Crystal Fuel

--- Assets/Ship/FuelSystem/FuelManager.cs
+++ Assets/Ship/FuelSystem/FuelManager.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class FuelManager : MonoBehaviour
 {
+    [SerializeField] private List<Sprite> fuelSprites = new List<Sprite>();
     [SerializeField] private GameObject fuelTemplatePrefab;
-    [SerializeField] private List<GameObject> fuels;
+    [SerializeField] private List<GameObject> fuels = new List<GameObject>();
     [SerializeField] private float fuelRespawnTime = 0;
     private float currentFuelRespawnTime = 0;
     private int currentFuelObjectIndex = 0;
 
     void Awake()
     {
-        fuels = new List<GameObject>();
-        //fuels.Resize(10);
         for(int i = 0; i < 10; ++i)
         {
             fuels.Add(Instantiate(fuelTemplatePrefab));
@@ -36,6 +35,8 @@ public class FuelManager : MonoBehaviour
     {
         if (!fuels[currentFuelObjectIndex].activeSelf)
         {
+            fuelSprites.Shuffle();
+            fuels[currentFuelObjectIndex].GetComponentInChildren<SpriteRenderer>().sprite = fuelSprites[0];
             fuels[currentFuelObjectIndex].SetActive(true);
             fuels[currentFuelObjectIndex].transform.position = transform.position;
         }
