192aff7c0a9ef75171ea986e244152e8500577be
Reynald Brassard <reynaldbrassard599@gmail.com>
2020-12-10 21:33:17 -0800

    Refactored fuel system

---
+++ Assets/Ship/FuelSystem/EngineBurner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineBurner : MonoBehaviour
+{
+    [SerializeField] private float engineMaxHeat;
+    [SerializeField] private float fuelRestoreHeatAmount;
+
+    // Start is called before the first frame update
+    private float currentEngineHeat;
+    void Awake()
+    {
+        currentEngineHeat = engineMaxHeat;
+    }
+
+    public float FuelPercentage { get { return currentEngineHeat / engineMaxHeat; } }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentEngineHeat -= Time.fixedDeltaTime;
+        if (currentEngineHeat <= 0)
+        {
+            FindObjectOfType<GameManager>().StartGameOver();
+        }
+    }
+    public void BurnFuel()
+    {
+        if (currentEngineHeat + fuelRestoreHeatAmount >= engineMaxHeat)
+        {
+            currentEngineHeat = engineMaxHeat;
+        }
+        else
+        {
+            currentEngineHeat = currentEngineHeat + fuelRestoreHeatAmount;
+        }
+    }
+}
