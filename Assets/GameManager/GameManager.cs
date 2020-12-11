9ea49504a2f57ed05c761f7b2e5c7eccc0cafd97
Reynald Brassard <reynaldbrassard599@gmail.com>
2020-12-10 19:14:45 -0800

    Changed gamemanager prefab

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] MalfunctionManager malfunctionManager;
+    MalfunctionManager malfunctionManager;
     [SerializeField] int maxMalfunctions = 5;
     [SerializeField] float timeToComplete = 5 * 60; // 5 minutes
 
@@ -15,9 +15,13 @@ public class GameManager : MonoBehaviour
     public event GameCondition OnGameOver;
 
     public float CurrentTravelProgress { get { return Mathf.Clamp(currentTime / timeToComplete, 0.0f, 1.0f); } }
-    public float CriticalMalfunctionProgress { get { return Mathf.Clamp((float)malfunctionManager.CriticalMalfunctionCount / maxMalfunctions, 0.0f, 1.0f); } } 
-   
-    void Update()
+    public float CriticalMalfunctionProgress { get { return Mathf.Clamp((float)malfunctionManager.CriticalMalfunctionCount / maxMalfunctions, 0.0f, 1.0f); } }
+
+	private void Start()
+	{
+        malfunctionManager = GetComponent<MalfunctionManager>();
+	}
+	void Update()
     {
         currentTime += Time.deltaTime;
 
