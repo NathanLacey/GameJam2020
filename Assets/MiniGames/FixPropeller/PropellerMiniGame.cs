22d85d8730b018e5e83fca031fc4b37d013b038d
NathanLacey <nathan.lacey@hotmail.com>
2020-12-10 16:58:46 -0800

    Propeller info

--- Assets/MiniGames/FixPropeller/PropellerMiniGame.cs
+++ Assets/MiniGames/FixPropeller/PropellerMiniGame.cs
@@ -6,31 +6,53 @@ public class PropellerMiniGame : MonoBehaviour, IMiniGame
 {
     [SerializeField] SpinPropeller WorkingPropeller;
     [SerializeField] FixPropeller BrokenPropeller;
-
+	[SerializeField] GameObject Arrows;
     public bool IsFinished { get { return BrokenPropeller.IsFinished; } }
-
+	private bool FlashInfo = false;
+	float FlashMaxSeconds = 2.0f;
+	float CurrentSeconds = 0.0f;
 	private void Update()
 	{
 		if(IsFinished)
 		{
 			BrokenPropeller.gameObject.SetActive(false);
+			Arrows.gameObject.SetActive(false);
 			WorkingPropeller.gameObject.SetActive(true);
 		}
+
+		if(FlashInfo)
+		{
+			CurrentSeconds += Time.fixedDeltaTime;
+			if(CurrentSeconds >= FlashMaxSeconds)
+			{
+				Flash();
+				CurrentSeconds = 0.0f;
+			}
+		}
+	}
+
+	void Flash()
+	{
+		Arrows.gameObject.SetActive(!Arrows.gameObject.activeSelf);
 	}
 
 	void Reset()
 	{
 		BrokenPropeller.gameObject.SetActive(true);
+		Arrows.gameObject.SetActive(true);
 		WorkingPropeller.gameObject.SetActive(false);
 	}
 
 	public void StartMiniGame()
 	{
+		FlashInfo = false;
+		Arrows.gameObject.SetActive(false);
 		BrokenPropeller.GetComponent<DragAndSpin>().enabled = true;
 	}
 
 	public void OnMalfunctionStart()
 	{
+		FlashInfo = true;
 		Reset();
 	}
 }
