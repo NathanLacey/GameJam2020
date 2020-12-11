ce2bbd7cd2b84324a77cb290f56450fa69253566
Reynald Brassard <reynaldbrassard599@gmail.com>
2020-12-11 11:30:05 -0800

    Added Pause menu

---
+++ Assets/HUD/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] InputAction inputAction;
+    [SerializeField] GameObject pauseMenu;
+
+	private void Start()
+	{
+		inputAction.Enable();
+		
+	}
+	void OnPauseMenu()
+	{
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+	}
+
+	private void Update()
+	{
+		if (inputAction.triggered)
+		{
+			OnPauseMenu();
+		}
+	}
+}
---
+++ Assets/Player/VolumeControl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+	public void OnChanged(float value)
+	{
+		AudioListener.volume = value;
+	}
+}
