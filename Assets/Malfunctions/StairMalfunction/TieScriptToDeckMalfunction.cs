8aa74317a11051215758c2e71d949d21a6ed7e78
Jake Roman-Barnes <bowenrombar@gmail.com>
2020-12-10 20:42:34 -0800

    Show malfunctions near stairs for other decks

---
+++ Assets/Malfunctions/StairMalfunction/TieScriptToDeckMalfunction.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TieScriptToDeckMalfunction : MonoBehaviour
+{
+	enum DeckLevel
+	{
+		Top,
+		Bottom
+	}
+	[SerializeField] MonoBehaviour controlledScript;
+	[SerializeField] DeckLevel deckLevel;
+	MalfunctionManager malfunctionManager = null;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		malfunctionManager = GameObject.FindObjectOfType<MalfunctionManager>();
+		TrySetControlledEnableState();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		TrySetControlledEnableState();
+	}
+
+	void TrySetControlledEnableState()
+	{
+		if (!malfunctionManager)
+		{
+			return;
+		}
+		switch (deckLevel)
+		{
+			case DeckLevel.Top:
+				{
+					controlledScript.enabled = malfunctionManager.hasBottomDeckMalfunctions;
+					break;
+				}
+			case DeckLevel.Bottom:
+				{
+					controlledScript.enabled = malfunctionManager.hasTopDeckMalfunctions;
+					break;
+				}
+			default:
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/Helpers/FlashSprites.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashSprites : MonoBehaviour
+{
+	[SerializeField] float flashIntervalSeconds = 2.0f;
+	float elapsedSeconds = 0.0f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		elapsedSeconds += Time.deltaTime;
+		if (elapsedSeconds >= flashIntervalSeconds)
+		{
+			SpriteRenderer[] renderers = GetComponentsInChildren<SpriteRenderer>();
+			foreach(SpriteRenderer sprite in renderers)
+			{
+				sprite.enabled = !sprite.enabled;
+			}
+			elapsedSeconds = 0.0f;
+		}
+	}
+}
