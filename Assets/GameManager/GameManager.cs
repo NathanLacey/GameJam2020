2801b542f5364efccde94e3560a9ff721d8a3abc
Jake Roman-Barnes <bowenrombar@gmail.com>
2020-12-10 23:08:23 -0800

    Added score to gamer over screen

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -4,47 +4,66 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    MalfunctionManager malfunctionManager;
-    [SerializeField] int maxMalfunctions = 5;
-    [SerializeField] float timeToComplete = 5 * 60; // 5 minutes
+	MalfunctionManager malfunctionManager;
+	[SerializeField] int maxMalfunctions = 5;
+	[SerializeField] float timeToComplete = 5 * 60; // 5 minutes
+	bool isGameOver = false;
 
-    float currentTime = 0.0f;
+	float currentTime = 0.0f;
+	int currentScore = 0;
 
-    public delegate void GameCondition();
-    public event GameCondition OnGameComplete;
-    public event GameCondition OnGameOver;
+	public delegate void GameCondition();
+	public event GameCondition OnGameComplete;
+	public event GameCondition OnGameOver;
 
-    public float CurrentTravelProgress { get { return Mathf.Clamp(currentTime / timeToComplete, 0.0f, 1.0f); } }
-    public float CriticalMalfunctionProgress { get { return Mathf.Clamp((float)malfunctionManager.CriticalMalfunctionCount / maxMalfunctions, 0.0f, 1.0f); } }
+	public float CurrentTravelProgress { get { return Mathf.Clamp(currentTime / timeToComplete, 0.0f, 1.0f); } }
+	public int CurrentScore
+	{
+		get
+		{
+			if(!isGameOver)
+			{
+				currentScore = (int)(CurrentTravelProgress * 100000.0f);
+			}
+			return currentScore;
+		}
+	}
+	public float CriticalMalfunctionProgress { get { return Mathf.Clamp((float)malfunctionManager.CriticalMalfunctionCount / maxMalfunctions, 0.0f, 1.0f); } }
 
 	private void Start()
 	{
-        malfunctionManager = GetComponent<MalfunctionManager>();
+		malfunctionManager = GetComponent<MalfunctionManager>();
+		OnGameOver += SetGameOver;
 	}
 	void Update()
-    {
-        currentTime += Time.deltaTime;
+	{
+		currentTime += Time.deltaTime;
 
-        if(currentTime >= timeToComplete)
+		if (currentTime >= timeToComplete)
 		{
-            OnGameComplete?.Invoke();
+			OnGameComplete?.Invoke();
 		}
 
-        if(malfunctionManager != null)
+		if (malfunctionManager != null)
 		{
-            if(malfunctionManager.CriticalMalfunctionCount >= maxMalfunctions)
+			if (malfunctionManager.CriticalMalfunctionCount >= maxMalfunctions)
 			{
-                StartGameOver();
+				StartGameOver();
 			}
 		}
 		else
 		{
-            Debug.Log("Must Attach Malfunction Manager");
+			Debug.Log("Must Attach Malfunction Manager");
 		}
-    }
+	}
 
-    public void StartGameOver()
+	public void StartGameOver()
 	{
-        OnGameOver?.Invoke();
+		OnGameOver?.Invoke();
+	}
+
+	void SetGameOver()
+	{
+		isGameOver = true;
 	}
 }
